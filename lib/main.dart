647431305c47be0fec094e10d2794d95506049e5
TOSHIBA <hazeefanasir22@gmail.com>
2024-07-06 12:59:55 +0500

    hy

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:instant_language_trans/main.dart%20for%20login.dart';
+import 'package:instant_language_trans/main_page.dart';
+import 'auth_page.dart';
+import 'firebase_options.dart';
+import 'home.dart';
+
+void main()
+  async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(const MyApp());
+  }
+
+mixin currentPlatform {
+}
+
+class DefaultFirebaseOptions {
+  static var currentPlatform;
+}
+
+class Firebase {
+  static initializeApp({required options}) {}
+}
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instant Translator App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: AuthPage(),
+    );
+  }
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:instant_language_trans/auth_page.dart';
+import 'package:instant_language_trans/main.dart%20for%20login.dart';
+import 'auth_page.dart';
+import 'home_page.dart';
+import 'login_page.dart';
+import 'login_page.dart';
+
+class MainPage extends StatelessWidget {
+const MainPage({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+body: StreamBuilder<User >(
+stream: FirebaseAuth.instance.authStateChanges(),
+builder: (context, snapshot) {
+if (snapshot.hasData) {
+return HomePage();
+} else {
+return AuthPage();
+}
+},
+),
+);
+}
+}
+
+class User {
+}
